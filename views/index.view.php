4e31dc7add06a7eca3360539859ef2cea71ca86d
Jan Christlieb <mail@janchristlieb.de>
2019-02-25 07:09:38 +0100

    add tab view for tasks

--- views/index.view.php
+++ views/index.view.php
@@ -15,16 +15,28 @@ require 'snippets/head.php'
 
 <main class="container">
     <div class="row">
-        <div class="col-12 col-md-6 pb-4">
+        <div class="col-12 col-md-8 pb-4">
             <h2>My tasks</h2>
             <hr width="50%" align="left">
+            <ul class="nav nav-tabs" id="taskTabs" role="tablist">
+                <li class="nav-item"><a class="nav-link" id="active-tab" data-toggle="tab" role="tab" href="#active">active</a></li>
+                <li class="nav-item"><a class="nav-link" id="inactive-tab" data-toggle="tab" role="tab" href="#inactive">inactive</a></li>
+                <li class="nav-item"><a class="nav-link" id="all-tab" data-toggle="tab" role="tab" href="#all">all</a></li>
+            </ul>
+
+            <div class="tab-content" id="taskTabsContent">
+                <div class="" id="" role=""
+            </div>
+
+
+
             <ul>
                 <?php foreach ($tasks as $task) {
                     echo '<li>' . $task['title'] . '</li>';
                 } ?>
             </ul>
         </div>
-        <div class="col-12 col-md-6 pb-4">
+        <div class="col-12 col-md-4 pb-4">
             <h2>New task</h2>
             <hr width="50%" align="left">
             <form>
